ad096983ea52fcd5a6433e86d323c2e10012d8bb
Harry Sadler <harrysadlermusic@gmail.com>
2022-02-20 11:43:03 +0100

    add basic Golang http server, add docker files for local development

---
+++ GameServer/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.17
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
---
+++ GameServer/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+	// run the server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	addr := flag.String("addr", "0.0.0.0:"+port, "http service address")
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("Server start error:", err)
+	}
+}
