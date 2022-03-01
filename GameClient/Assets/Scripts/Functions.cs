2b5c1088194ffac7d2b8502eaed8cf6eecee24ed
Harry Sadler <harrysadlermusic@gmail.com>
2022-03-01 23:39:27 +0100

    get player-client consumption of central-client streamed positions working, add smoothing of player-client ball movement

---
+++ GameClient/Assets/Scripts/Functions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Functions
+{
+
+    public static Vector3 StringToVector3(string sVector)
+    {
+        // stolen from: https://answers.unity.com/questions/1134997/string-to-vector3.html
+        // Remove the parentheses
+        if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+        // split the items
+        string[] sArray = sVector.Split(',');
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2])
+        );
+        return result;
+    }
+
+}
--- GameClient/Assets/Scripts/GameManagerScript.cs
+++ GameClient/Assets/Scripts/GameManagerScript.cs
@@ -6,28 +6,65 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
 
-    public ServerConn serverConn;
+    [NonSerialized]
     public bool isCentralClient;
+    public ServerConn serverConn;
+
+    public GameObject gameBallPrefab;
 
     private GameObject gameBall;
+    private Vector3 newGameBallPosition = Vector3.zero;
+    private Vector3 gameBallVelocity = Vector3.zero;
 
 
     // UNITY HOOKS
 
     void Awake()
     {
+        this.isCentralClient = Environment.GetEnvironmentVariable("CENTRAL_GAME_CLIENT") == "1";
         this.serverConn = new ServerConn(Constants.GAME_SERVER_URL);
         this.serverConn.RegisterServerMessageHandler(this.ServerMessageHandler);
-        this.isCentralClient = Environment.GetEnvironmentVariable("CENTRAL_GAME_CLIENT") == "1";
     }
 
     void Start()
     {
-        this.gameBall = GameObject.FindGameObjectWithTag("Ball");
+        this.gameBall = GameObject.Instantiate(this.gameBallPrefab, Vector3.zero, Quaternion.identity);
+        if(this.isCentralClient)
+        {
+            this.gameBall.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
     }
 
     void Update()
     {
+        if (this.isCentralClient)
+        {
+            this.serverConn.SendClientMessageToServer(this.gameBall.transform.position.ToString());
+        }
+        else if (this.gameBall.transform.position != this.newGameBallPosition)
+        {
+            // smooth movement of central game-client controlled ball position
+            var d = Vector3.Distance(this.gameBall.transform.position, this.newGameBallPosition);
+            if (d < 1f)
+            {
+                this.gameBall.transform.position = Vector3.SmoothDamp(
+                    this.gameBall.transform.position,
+                    this.newGameBallPosition,
+                    ref this.gameBallVelocity,
+                    0.01F
+                );
+                // alt smoothing method (seems choppier)
+                //this.gameBall.transform.position = Vector3.MoveTowards(
+                //    this.gameBall.transform.position,
+                //    this.newGameBallPosition,
+                //    100 * Time.deltaTime
+                //);
+            }
+            else
+            {
+                this.gameBall.transform.position = this.newGameBallPosition;
+            }
+        }
         
     }
 
@@ -37,7 +74,7 @@ public class GameManagerScript : MonoBehaviour
 
     private void ServerMessageHandler(string serverMessage)
     {
-        //Debug.Log("Server message received: " + serverMessage);
+        this.newGameBallPosition = Functions.StringToVector3(serverMessage);
     }
 
 }
